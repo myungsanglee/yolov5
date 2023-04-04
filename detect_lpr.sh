c6efe6dfd77d030dd2ee3160527d69480f018068
myungsanglee <lee0577@naver.com>
2023-04-04 11:12:37 +0900

    update

--- detect_lpr.sh
+++ detect_lpr.sh
@@ -1,8 +1,8 @@
 python detect_lpr.py \
 --weights 'runs/train/exp2/weights/best.pt' \
---source '/home/fssv2/myungsang/datasets/lpr/test_2.txt' \
+--source '/home/fssv2/myungsang/datasets/lpr/test_v1.txt' \
 --data data/lpr.yaml \
 --img 224 \
 --conf-thres 0.4 \
 --iou-thres 0.5 \
---device 'cpu'
+--device '0'
